d23b0bb651343298d54608c6e4294aadfb58b78a
Henry Witcher <witcher.henryallen@gmail.com>
2020-03-12 12:26:09 +0530

    Update Master.r

--- Master.r
+++ Master.r
@@ -1,4 +1,108 @@
-## Classification ##
+## cmeans ##
+
+library(clv)
+m<-2
+data <- read.csv("~/iris.csv")
+points<-nrow(data)
+dimensions<-ncol(data)-1
+membershipMatrix<-matrix(nrow=points,ncol=dimensions)
+optimalCluster<-function(){
+  dunn1<-vector()
+  db1<-vector()
+  for(i in 2:10){
+    agnesMod<-agnes(data[-ncol(data)])
+    vPred<-as.integer(cutree(agnesMod,i))
+    clsScatter<-cls.scatt.data(data[-ncol(data)],vPred,"euclidean")
+    dunn1<-append(dunn1,clv.Dunn(clsScatter,"centroid","centroid"))
+    db1<-append(db1,clv.Davies.Bouldin(clsScatter,"centroid","centroid"))
+  }
+  plot(c(2:10),dunn1,xlab="No of clusters",ylab="Dunn Index",type="o",main="Dunn Index Evaluation")
+  plot(c(2:10),db1,xlab="No of clusters",ylab="Db Index",type="o",main="Db Index Evaluation")
+  
+}
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+distance<-function(centroids,clusters){
+  distanceMatrix<-matrix(nrow=points,ncol=clusters)
+  for(i in 1:points){
+    for(j in 1:clusters){
+      dist <- sqrt( sum( ( data[i,-ncol(data)] - centroids[j,] )^2 ) )
+      distanceMatrix[i,j]<-dist
+    }
+  } 
+  return(distanceMatrix)
+}
+membership<-function(distanceMatrix,clusters){
+  
+  membershipMatrix<-matrix(0,nrow=points,ncol=clusters)
+  for(i in 1:nrow(distanceMatrix)){
+    for(j in 1:ncol(distanceMatrix)){
+      up=(1/distanceMatrix[i,j])^(1/(m-1))
+      down=0
+      for(x in 1:ncol(distanceMatrix)){
+        down=down+(1/distanceMatrix[i,x])
+      }
+      down1=down^(1/(m-1))
+      membershipMatrix[i,j]<-up/down1
+      if(is.nan(membershipMatrix[i,j])){
+        membershipMatrix[i,j]=0;
+      }
+      
+    }
+  }
+  return(membershipMatrix)
+}
+calculateCentroid<-function(membershipMatrix,clusters){
+  centroid<-matrix(nrow=clusters,ncol=dimensions)
+  for(i in 1:nrow(centroid)){
+    for(j in 1:ncol(centroid)){
+      centroid[i,j]<-( sum(data[,j] * membershipMatrix[,i]^m)/sum(membershipMatrix[,i]^m) )
+      centroid[i,j]<-round(centroid[i,j],3)
+    }
+  }
+  return(centroid)
+}
+##for(i in 1:(ncol(data)-1)){
+##  data[,i]=normalize(data[,i])
+##}
+cMeans<-function(clusters){
+  prevCentroids=matrix(nrow=clusters,ncol=dimensions)
+   for(i in 1:nrow(prevCentroids)){
+     for(j in 1:ncol(prevCentroids))
+        prevCentroids[i,j]<-round(runif(1,min(data[,j]),max(data[,j])),4)
+  }
+  centroids=prevCentroids
+  flag<-TRUE
+  while(flag){
+    distanceMatrix<-distance(centroids,clusters)
+    memberShipMatrix<-membership(distanceMatrix,clusters)
+    centroids<-calculateCentroid(memberShipMatrix,clusters)
+    if(identical(prevCentroids,centroids)){
+      flag=FALSE
+    }
+    else{
+      prevCentroids=centroids
+    }
+    i=i+1
+  }
+  print("FINAL CENTROIDS")
+  print(centroids)
+  clusterOutput<-vector()
+  for(i in 1:nrow(memberShipMatrix)){
+    clusterOutput=append(clusterOutput,which.max(memberShipMatrix[i,]))
+  }
+  plot(iris[c(1, 2)], col = data[,ncol(data)],main="Original clustered output")
+  plot(iris[c(1, 2)], col = clusterOutput,main="CMeans clustered output")
+  points(centroids[,c(1, 2)], col = 1:length(unique(clusterOutput)), pch = 8, cex = 2)
+}
+par(mfrow=c(2,2))
+optimalCluster()
+cMeans(3)
+
+
+## classification ##
+
 
 library(caret)
 library(e1071)
@@ -104,6 +208,25 @@ y1 <- factor(expectedOutput)
 y2 <- factor(output)
 cm = confusionMatrix(y1,y2)
 print(cm)
+cat("Accuracy",cm$overall["Accuracy"],"\n")
+if(is.na(cm$byClass['Pos Pred Value'])){
+  cat("Precision",0,"\n" )
+}else{
+  cat("Precision",cm$byClass['Pos Pred Value'],"\n" )
+}
+if(is.na(cm$byClass['Sensitivity'])){
+  cat("Recall",0,"\n")
+}else{
+  cat("Recall",cm$byClass['Sensitivity'],"\n")
+}
+if(is.na(cm$byClass['Pos Pred Value'])||is.na(cm$byClass['Sensitivity'])){
+  cat("F Measure",0,"\n")
+}else{
+  Precision=cm$byClass['Pos Pred Value']
+  Recall=cm$byClass['Sensitivity']
+  Fmeasure=(2 * Precision * Recall) / (Precision + Recall)
+  cat("F Measure",Fmeasure,"\n")
+}
 par(mfrow=c(1,2))
 pie(x,labels,main="Count of records in each Class",col=rainbow(length(x)))
 plot(data[,1],data[,2],xlab="Sepal Length",ylab="Sepal Width",col=output,main="Sepal Length vs SepalWidth",pch=2)
@@ -111,7 +234,7 @@ legend("topleft",
        legend = labels, col=c("black","red","green"),pch=2)
 
 
-## Mamdani Model ##
+## mamdani ##
 
 library(frbs)
 library(caret)
@@ -134,7 +257,7 @@ type.snorm <- "MAX"
 type.implication.func <- "ZADEH"
 name <- "Sim-0"
 #newdata<- matrix(c(30, 75,  45, 80, 30, 45), nrow= 3, byrow = TRUE)
-newdata<-read.csv("~/dirt.csv")
+newdata<-read.csv("~/dirty.csv")
 colnames.var <- c("dirtiness", "type of dirt", "washing time")
 num.fvaloutput <- matrix(c(5), nrow=1)
 varoutput.1 <- c("very_short", "short", "medium","long","very_long")
@@ -154,10 +277,10 @@ rule <- matrix(c("large","and","greasy","->","very_long",
                  "small","and","medium_type","->","medium",
                  "large","and","not_greasy","->","medium",
                  "medium_dirtiness","and","not_greasy","->","short",
-                 "small","and","not_greasy","->","very_short"),
-               nrow=9, byrow=TRUE)
-object <- frbs.gen(range.data, num.fvalinput, names.varinput, num.fvaloutput, varout.mf,
-                   names.varoutput, rule, varinp.mf, type.model, type.defuz, type.tnorm,
+                 "small","and","not_greasy","->","very_short"), 
+               nrow=9, byrow=TRUE) 
+object <- frbs.gen(range.data, num.fvalinput, names.varinput, num.fvaloutput, varout.mf, 
+                   names.varoutput, rule, varinp.mf, type.model, type.defuz, type.tnorm, 
                    type.snorm, func.tsk = NULL, colnames.var, type.implication.func, name)
 par(mar=c(1,1,1,1))
 summary(object)
@@ -166,61 +289,60 @@ plotMF(object)
 # data[,i]=normalize(data[,i],0,1)
 #}
 res <- predict(object, newdata[,-ncol(newdata)])$predicted.val
+print("Defuzzified values")
 print(res)
 label<-vector()
 labelEncoding<-vector()
 for(i in 1:length(res)){
   for(j in 1:ncol(varout.mf)){
-    lowerbound=varout.mf[2,j]
-    upperbound=varout.mf[4,j]
-    if(res[i]<=upperbound && res[i]>lowerbound ){
-      label=append(label,varoutput.1[j])
-      labelEncoding=append(labelEncoding,j)
-    }
+      lowerbound=varout.mf[2,j]
+      upperbound=varout.mf[4,j]
+      if(res[i]<=upperbound && res[i]>lowerbound ){
+        label=append(label,varoutput.1[j])
+        labelEncoding=append(labelEncoding,j)
+      }
   }
 }
+print("Associated Classes")
 print(label)
 output<-factor(label)
 expectedOutput<-factor(newdata[,ncol(newdata)])
 levels(expectedOutput)<-varoutput.1
 levels(output)<-varoutput.1
-cm=confusionMatrix(newdata[,ncol(newdata)],output)
+cm=confusionMatrix(expectedOutput,output)
 print(cm)
 cat("Accuracy",cm$overall["Accuracy"],"\n")
 if(is.na(cm$byClass['Pos Pred Value'])){
   cat("Precision",0,"\n" )
+}else{
+  cat("Precision",cm$byClass['Pos Pred Value'],"\n" )
 }
 if(is.na(cm$byClass['Sensitivity'])){
   cat("Recall",0,"\n")
+}else{
+  cat("Recall",cm$byClass['Sensitivity'],"\n")
 }
 if(is.na(cm$byClass['Pos Pred Value'])||is.na(cm$byClass['Sensitivity'])){
   cat("F Measure",0,"\n")
+}else{
+ 
+    Precision=cm$byClass['Pos Pred Value']
+    Recall=cm$byClass['Sensitivity']
+    Fmeasure=(2 * Precision * Recall) / (Precision + Recall)
+    cat("F Measure",Fmeasure,"\n")
+  
 }
+Sys.sleep(10)
+par(mfrow=c(1,1))
 plot(newdata[,1],newdata[,2],xlab="dirtiness",ylab="Type of dirt",col=labelEncoding,main="Dirtiness vs DirtType",pch=2)
-legend("topleft",
-       legend = varoutput.1, col=labelEncoding,pch=2)
-
-dirt.csv
-dirtiness,type of dirt,washing_time
-30,75,medium
-45,80,long
-30,45,medium
-55,85,very_long
-25,23,very_short
-65,25,short
-15,32,very_short
-80,80,long
-90,90,long
-5,95,medium
-67,78,medium
-77,11,short
-17,24,very_short
-6,40,short
+legend("topright",
+      legend = varoutput.1, col=1:length(varoutput.1),pch=2,inset=c(0,0.35))
 
 
-## Sugeno Model ##
+## sugeno ##
 
 library(frbs)
+library(caret)
 #use if needed
 normalize <- function(x,new_min,new_max) {
   return (((x - min(x)) / (max(x) - min(x)))*(new_max-new_min)+new_min)
@@ -239,7 +361,7 @@ type.tnorm <- "MIN"
 type.snorm <- "MAX"
 type.implication.func <- "ZADEH"
 name <- "Sim-0"
-newdata<- read.csv("tsk.csv")
+newdata<- read.csv("~/tsk.csv")
 colnames.var <- c("W", "T","AU")
 type.model <- "TSK"
 num.fvaloutput <- matrix(c(2), nrow=1)
@@ -247,8 +369,8 @@ varoutput.1 <- c("small","large")
 names.varoutput <- c(varoutput.1)
 varout.mf <- matrix(c(1, 2000, 6000, 10000,NA ,
                       1, 10000, 12500, 15000, NA
-),
-nrow = 5, byrow = FALSE)
+                     ),
+                    nrow = 5, byrow = FALSE)
 func.tsk <- matrix(c(4.6925, -526.2, 2631, 3.4765, -210.5, 2103, 4.6925, -526.2, 2631),
                    nrow = 3, byrow = TRUE)
 rule <- matrix(c("largeW", "and", "smallT", "->",
@@ -259,12 +381,12 @@ object <- frbs.gen(range.data, num.fvalinput, names.varinput,
                    num.fvaloutput = NULL, varout.mf = NULL, names.varoutput = NULL, rule,
                    varinp.mf, type.model, type.defuz = NULL, type.tnorm, type.snorm,
                    func.tsk, colnames.var, type.implication.func, name)
+par(mfrow=c(2,2))
 plotMF(object)
 #for(i in 1:(ncol(data)-1)){
-# data[,i]=normalize(data[,i],0,1)
+ # data[,i]=normalize(data[,i],0,1)
 #}
 res <- predict(object, newdata[,-ncol(newdata)])$predicted.val
-print(res)
 
 label<-vector()
 labelEncoding<-vector()
@@ -278,124 +400,38 @@ for(i in 1:length(res)){
     }
   }
 }
+print("Defuzzified values ")
+print(res)
+print("Associated Class")
+print(label)
 output<-factor(label)
 expectedOutput<-factor(newdata[,ncol(newdata)])
 levels(expectedOutput)<-varoutput.1
 levels(output)<-varoutput.1
 cm=confusionMatrix(expectedOutput,output)
+print("Confusion Matrix")
 print(cm)
 cat("Accuracy",cm$overall["Accuracy"],"\n")
 if(is.na(cm$byClass['Pos Pred Value'])){
   cat("Precision",0,"\n" )
+}else{
+  cat("Precision",cm$byClass['Pos Pred Value'],"\n" )
 }
 if(is.na(cm$byClass['Sensitivity'])){
   cat("Recall",0,"\n")
+}else{
+  cat("Recall",cm$byClass['Sensitivity'],"\n")
 }
 if(is.na(cm$byClass['Pos Pred Value'])||is.na(cm$byClass['Sensitivity'])){
   cat("F Measure",0,"\n")
+}else{
+  Precision=cm$byClass['Pos Pred Value']
+  Recall=cm$byClass['Sensitivity']
+  Fmeasure=(2 * Precision * Recall) / (Precision + Recall)
+  cat("F Measure",Fmeasure,"\n")
 }
-
-plot(newdata[,1],newdata[,2],xlab="W",ylab="T",col=labelEncoding,main="W vs T",pch=2)
-legend("topleft",
-       legend = varoutput.1, col=labelEncoding,pch=2)
-
-
-
-''' w,t,class
-1300,6.5,small
-1200,12.5,small
-1500,13,large
-1300,12,large
-1600,12.5,small
-1100,4.5,small
-1800,12,large
-1700,10,large
-1280,7,small
-1100,14,large '''
-
-
-## C Means ##
-
-library(philentropy)
-it=1
-eps=3
-m=2
-no_of_clusters=3
-no_of_dimensions=4
-no_of_datapoints=150
-
-datapoints<-c()
-#for(i in 1:no_of_dimensions){
-#  datapoints<-cbind(datapoints,round(runif(no_of_datapoints,min=1,max=10)))
-#}
-data(iris)
-datapoints=cbind(iris[,1],iris[,2],iris[,3],iris[,4])
-
-#step1
-clusters<-c()
-for(i in 1:no_of_clusters){
-  clusters<-cbind(clusters,runif(no_of_datapoints))
-}
-for(i in 1:nrow(clusters)){
-  clusters[i,]=clusters[i,]/sum(clusters[i,])
-}
-clusters
-
-#datapoints<-cbind(c(1,2,3,4,5,6),c(6,5,8,4,7,9))
-#clusters<-cbind(c(0.8,0.9,0.7,0.3,0.5,0.2),c(0.2,0.1,0.3,0.7,0.5,0.8))
-centroid<-c()
-
-repeat{
-  
-  #step2
-  prev_centroid=centroid
-  centroid<-c()
-  for(i in 1:no_of_clusters){
-    temp<-c()
-    for(j in 1:no_of_dimensions){
-      temp<-c(temp,sum((clusters[,i]^m)*datapoints[,j])/sum(clusters[,i]^m))
-    }
-    centroid<-rbind(centroid,temp)
-  }
-  rownames(centroid)<-c()
-  centroid
-  
-  #step3
-  dissimilarity<-c()
-  for(i in 1:no_of_clusters){
-    temp<-c()
-    for(j in 1:no_of_datapoints){
-      temp<-rbind(temp,distance(rbind(centroid[i,],datapoints[j,]),method="euclidean"))
-    }
-    dissimilarity<-cbind(dissimilarity,temp)
-  }
-  colnames(dissimilarity)<-c()
-  dissimilarity
-  
-  #step4
-  for(i in 1:nrow(dissimilarity)){
-    dissimilarity[i,]=((1/dissimilarity[i,])/sum(1/dissimilarity[i,]))
-  }
-  dissimilarity
-  
-  cat('Iteration',it,'\n')
-  print(cbind(datapoints,clusters,dissimilarity))
-  #plot(1:150,rowMax(dissimilarity))
-  
-  clusters=dissimilarity
-  
-  #exit condition
-  it=it+1
-  if(it==3){
-    flag=0
-    for(i in 1:no_of_clusters){
-      if(distance(rbind(centroid[i,],prev_centroid[i,]),method="euclidean")<eps){
-        flag=flag+1
-      }
-    }
-    if(flag==no_of_clusters){
-      break
-    }
-  }
-}
+Sys.sleep(10)
+plot(newdata[,1],newdata[,2],xlab="W",ylab="T",col=labelEncoding,main="W vs T",pch=1)
+legend("bottomright",
+       legend = varoutput.1, col=1:length(varoutput.1),pch=1,inset=c(0,0))
 
